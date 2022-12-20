1b0c8569fd888beb3cf7ee625b17ff2440d91528
OlgaLevochkina <olga.lyevochkina93@gmail.com>
2022-12-20 15:10:30 +0200

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-
-        public static void main(String[] args) {
-            for (int i = 0; i <= 10; i++) {
-                final int result = 5 * i;
-                System.out.println("5 * " + i + " = " + result);
-            }
-            System.out.println("");
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            final int result = 5 * i;
+            System.out.println("5 * " + i + " = " + result);
         }
+        System.out.println("");
     }
+}
